47ed1d416bad2f547518077d51e217d078dd3760
Infinity004 <ssrithesh@gmail.com>
2024-03-15 12:16:01 +0530

    modularized (rip spelling) the config

--- init.lua
+++ init.lua
@@ -1,24 +1,23 @@
 vim.cmd("set expandtab shiftwidth=4 tabstop=4 softtabstop=4 nu")
 vim.g.mapleader = " "
 
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins", {{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}})
+require("lazy").setup("plugins")
 
-vim.cmd.colorscheme "everforest"
+vim.cmd.colorscheme("everforest")
 
-vim.keymap.set('n', '<C-n>', '<Cmd>Neotree toggle<CR>')
+vim.keymap.set("n", "<C-n>", "<Cmd>Neotree toggle<CR>")
 
 require("mason").setup()
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,43 @@
+return {
+    -- lsps
+    {
+        "williamboman/mason.nvim",
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.clangd.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.jdtls.setup({
+                capabilities = capabilities,
+            })
+
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+        end,
+    },
+
+    -- formatters and linters
+    {
+        "nvimtools/none-ls.nvim",
+        config = function()
+            local null_ls = require("null-ls")
+            null_ls.setup({
+                sources = {
+                    null_ls.builtins.formatting.stylua,
+                    null_ls.builtins.formatting.black,
+                    null_ls.builtins.formatting.isort,
+                },
+            })
+            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+        end,
+    },
+}
